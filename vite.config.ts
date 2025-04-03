ed088d9b6abae98fe3b902e578638395f9381c70
Omar Elshamy <omar@elshamy.dev>
2025-04-03 17:36:58 +0200

    Add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import path from "path";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), netlifyPlugin()],
   publicDir: "./static",
   base: "./",
   server: {
